7c9fc5d69de024587ed4fc3c7fff0e3448f2ba02
吉井駿一 <syun_syun01@icloud.com>
2021-06-04 23:15:13 +0900

    モザイクの処理を実装

--- src/app/after-image/after-image.component.ts
+++ src/app/after-image/after-image.component.ts
@@ -19,11 +19,27 @@ export class AfterImageComponent implements OnInit {
     let size = 200
     let canvas = document.getElementById("canvas") as HTMLCanvasElement
     let ctx = canvas.getContext("2d")
-    ctx.fillStyle = "green"
-    ctx.fillRect(0,0,10,10)
+
+    let mosaicCanvas = document.getElementById("mosaic-canvas") as HTMLCanvasElement
+    let mosaicCtx = mosaicCanvas.getContext("2d")
+
     let img = new Image()
     img.src = this.src as string
     ctx.drawImage(img, 0,0, size, size)
-    this.afterSrc = canvas.toDataURL()
+
+    for (let x=0; x<size; x+=10){
+      for (let y=0; y<size; y+=10){
+        let imageData = ctx.getImageData(x,y,1,1)
+        let r = imageData.data[0]
+        let g = imageData.data[1]
+        let b = imageData.data[2]
+        let a = imageData.data[3]
+        mosaicCtx.fillStyle = "rgb(" + String(r) + "," + String(g) + "," + String(b) + ")"
+        mosaicCtx.fillRect(x,y,10,10)
+      }
+    }
+
+
+    this.afterSrc = mosaicCanvas.toDataURL()
   }
 }
