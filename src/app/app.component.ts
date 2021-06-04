3e50f88c9d63405dd68b5bab6d1bba0e60d2d306
吉井駿一 <syun_syun01@icloud.com>
2021-06-04 11:43:04 +0900

    inputで画像読み込み完了

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-mosaic';
+
+  file: File = null
+  imgSrc: string | ArrayBuffer = "";
+
+  constructor(){}
+
+  onChangeFileInput(event: any){
+
+    if (event.target.files.length === 0){
+      this.file = null;
+      this.imgSrc = ""
+      return;
+    }
+
+    let render = new FileReader();
+    this.file = event.target.files[0]
+    render.onload = () => {
+      this.imgSrc = render.result
+    }
+    render.readAsDataURL(this.file)
+  }
 }
