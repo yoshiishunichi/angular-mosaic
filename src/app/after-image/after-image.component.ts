4a07e163c6e1db3c60b4dd56993272cce888fd4a
吉井駿一 <syun_syun01@icloud.com>
2021-06-04 21:56:17 +0900

    canvas要素をimg要素に変換する処理を実装

--- src/app/after-image/after-image.component.ts
+++ src/app/after-image/after-image.component.ts
@@ -9,9 +9,21 @@ export class AfterImageComponent implements OnInit {
   
   @Input() src: string | ArrayBuffer
 
+  afterSrc = ""
+
+  param = 2
+
   constructor() { }
 
   ngOnInit(): void {
+    let size = 200
+    let canvas = document.getElementById("canvas") as HTMLCanvasElement
+    let ctx = canvas.getContext("2d")
+    ctx.fillStyle = "green"
+    ctx.fillRect(0,0,10,10)
+    let img = new Image()
+    img.src = this.src as string
+    ctx.drawImage(img, 0,0, size, size)
+    this.afterSrc = canvas.toDataURL()
   }
-
 }
